969072bedc7be658030f625c6310ee6df8e01b7c
Grant Davis <grantdavis303@gmail.com>
2024-03-21 12:23:47 -0600

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,88 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# rails db:seed
+
+Team.create!(
+    name: "Colorado Avalanche",
+    season_wins: 44,
+    playoff_eligible: true
+)
+
+Player.create!(
+    team_id: 1,
+    name: "Nathan MacKinnon",
+    jersey_number: 29,
+    won_cup: true
+)
+
+Player.create!(
+    team_id: 1,
+    name: "Mikko Rantenen",
+    jersey_number: 96,
+    won_cup: true
+)
+
+Player.create!(
+    team_id: 1,
+    name: "Casey Mittelstadt",
+    jersey_number: 37,
+    won_cup: false
+)
+
+Team.create!(
+    name: "Boston Bruins",
+    season_wins: 41,
+    playoff_eligible: true
+)
+
+Player.create!(
+    team_id: 2,
+    name: "Brad Marchand",
+    jersey_number: 63,
+    won_cup: true
+)
+
+Player.create!(
+    team_id: 2,
+    name: "Pat Maroon",
+    jersey_number: 61,
+    won_cup: true
+)
+
+Player.create!(
+    team_id: 2,
+    name: "Morgan Geekie",
+    jersey_number: 39,
+    won_cup: false
+)
+
+Team.create!(
+    name: "Carolina Hurricanes",
+    season_wins: 43,
+    playoff_eligible: true
+)
+
+Player.create!(
+    team_id: 3,
+    name: "Sebastian Aho",
+    jersey_number: 20,
+    won_cup: false
+)
+
+Player.create!(
+    team_id: 3,
+    name: "Jordan Staal",
+    jersey_number: 11,
+    won_cup: true
+)
+
+Player.create!(
+    team_id: 3,
+    name: "Brent Burns",
+    jersey_number: 8,
+    won_cup: false
+)
+
+puts "Seeded Successfully."
