425565b8ee1d96285678523a5ea26916111d840e
Grant Davis <grantdavis303@gmail.com>
2024-03-21 15:02:00 -0600

    Update RSpec Test

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,4 +4,29 @@ RSpec.describe Player do
 
     it {should belong_to :team}
 
+    describe 'player has attributes' do
+        it 'player has a name' do
+            test_team = Team.create!(name: "Colorado Avalanche", season_wins: 44, playoff_eligible: true)
+            test_team.players.create!(team_id: 1, name: "Nathan MacKinnon", jersey_number: 29, won_cup: true)
+
+            expect(test_team.players[0].name).to eq ("Nathan MacKinnon")
+        end
+
+        it 'player has a jersey number' do
+            test_team = Team.create!(name: "Colorado Avalanche", season_wins: 44, playoff_eligible: true)
+            test_team.players.create!(team_id: 1, name: "Nathan MacKinnon", jersey_number: 29, won_cup: true)
+
+            expect(test_team.players[0].jersey_number).to eq (29)
+        end
+
+        it 'player has won a Stanley Cup or not' do
+            test_team = Team.create!(name: "Colorado Avalanche", season_wins: 44, playoff_eligible: true)
+            test_team.players.create!(team_id: 1, name: "Nathan MacKinnon", jersey_number: 29, won_cup: true)
+            test_team.players.create!(team_id: 1, name: "Casey Mittelstadt", jersey_number: 37, won_cup: false)
+
+            expect(test_team.players[0].won_cup).to eq (true)
+            expect(test_team.players[1].won_cup).to eq (false)
+        end 
+    end
+
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -4,4 +4,26 @@ RSpec.describe Team do
 
     it {should have_many :players}
 
+    describe 'team has attributes' do
+        it 'team has a name' do
+            test_team = Team.create!(name: "Colorado Avalanche", season_wins: 44, playoff_eligible: true)
+            
+            expect(test_team.name).to eq ("Colorado Avalanche")
+        end
+
+        it 'team has a number of season wins' do
+            test_team = Team.create!(name: "Colorado Avalanche", season_wins: 44, playoff_eligible: true)
+            
+            expect(test_team.season_wins).to eq (44)
+        end
+
+        it 'team is playoff eligible or not' do
+            test_team_1 = Team.create!(name: "Colorado Avalanche", season_wins: 44, playoff_eligible: true)
+            test_team_2 = Team.create!(name: "San Jose Sharks", season_wins: 16, playoff_eligible: false)
+
+            expect(test_team_1.playoff_eligible).to eq (true)
+            expect(test_team_2.playoff_eligible).to eq (false)
+        end 
+    end
+
 end
