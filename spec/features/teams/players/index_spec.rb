d3b8f2ee810957f9ff3c4b9a4195b553e7c12123
Grant Davis <grantdavis303@gmail.com>
2024-03-26 17:48:10 -0600

    Final Changes

--- spec/features/teams/players/index_spec.rb
+++ spec/features/teams/players/index_spec.rb
@@ -53,23 +53,6 @@ RSpec.describe "teams players index page", type: :feature do
             expect(page).to have_content("17")
             expect(page).to have_content("Unfortunately not.")
         end
-
-        it 'players are sorted in alphabetical order' do
-            # Team.create!(name: "Colorado Avalanche", season_wins: 44, playoff_eligible: true)
-            
-            # visit '/teams'
-            # click_link "Colorado Avalanche"
-            # click_button "Colorado Avalanche Players"
-            # click_button "New Player"
-            # fill_in "player_name", with: "Grant Davis" 
-            # fill_in "jersey_number", with: "17"
-            # choose "no"
-            # click_button "Create Player"
-
-            # expect(page).to have_content("Grant Davis")
-            # expect(page).to have_content("17")
-            # expect(page).to have_content("Unfortunately not.")
-        end
     end
 
 end
